fd4e790cdd6c8a640634936c7d739535d74c58a3
magnusco <magnus.orke@lyse.net>
2019-03-22 23:24:51 +0100

    update

--- solution_class.py
+++ solution_class.py
@@ -3,6 +3,7 @@ import pickle
 from scipy.sparse import linalg
 from scipy import sparse
 import matplotlib.pyplot as plt
+from matplotlib import animation
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
@@ -38,14 +39,14 @@ class Nonlin_Scrodinger_Solver:
         return a * np.diag(e[1:], -1) + b * np.diag(e) + c * np.diag(e[1:], 1)
 
     def f1(self, x):
-        return np.sin(x)
+        # return np.sin(x)
+        return np.exp(1.0j * x)
 
     def analytic_1(self, x, t):
         X, T = np.meshgrid(x, t)
-        a = (np.cos(X) + 1.0j * np.sqrt(2) * np.sinh(T - np.pi)) / (np.sqrt(2) * np.cosh(T - np.pi) - np.cos(X))
-        return a * np.exp(1.0j * T)
+        return np.exp(1.0j * (X - (self.lmbda + 1) * T))
 
-    def plot_solution(self, sol, xi, ti, title):
+    def plot_solution(self, sol, xi, ti, title=None):
         fig = plt.figure(2)
         plt.clf()
         ax = fig.gca(projection='3d')
@@ -57,6 +58,12 @@ class Nonlin_Scrodinger_Solver:
         plt.ylabel('t')
         plt.title(title)
         plt.show()
+
+    def plot_analytic(self):
+        sol = self.analytic_1(self.xi, self.ti)
+        self.plot_solution(np.real(sol), self.xi, self.ti, "Real Part of Analytic Solution")
+        self.plot_solution(np.imag(sol), self.xi, self.ti, "Imaginary Part of Analytic Solution")
+        return 0
 
     def cn_explicit_average(self):
         self.sol[0, :] = self.f1(self.xi)
@@ -90,28 +97,52 @@ class Nonlin_Scrodinger_Solver:
         B_const[-1, 0] = - self.r / 2
 
         for i in range(0, self.N):
-            abs = np.absolute(self.sol[i, 0:-1])**2
-            B = np.diag(abs * 0.0j, 0) * self.lmbda * self.k * 0.5
+            abs = np.absolute(self.sol[i, 0:-1])
+            B = np.diag(abs**2 * 0.0j, 0) * self.lmbda * self.k * 0.5
             B += B_const
             b = np.matmul(B, self.sol[i, 0:-1])
         return 0
 
-    def cn_liearized(self):
+    def cn_liearized_1(self):
+        self.sol[0, :] = self.f1(self.xi)
+        A_const = self.tridiag(self.r / 2, 1.0j - self.r, + self.r / 2, self.M)
+        B_const = self.tridiag(- self.r / 2, 1.0j + self.r, - self.r / 2, self.M)
+        A_const[0, -1] = self.r / 2
+        A_const[-1, 0] = self.r / 2
+        B_const[0, -1] = - self.r / 2
+        B_const[-1, 0] = - self.r / 2
+
+        for i in range(0, self.N):
+            abs = np.absolute(self.sol[i, 0:-1])
+            A = np.diag(3 * abs**2 * 0.0j - 2 * abs * np.absolute(self.sol[i - 1, 0:-1] * 1.0j), 0)
+            B = np.diag(abs**2 * 0.0j, 0)
+            A *= 0.5 * self.lmbda * self.k
+            B *= 0.5 * self.lmbda * self.k
+            A += A_const
+            B += B_const
+            b = np.matmul(B, self.sol[i, 0:-1])
+            As = sparse.csr_matrix(A)
+            self.sol[i + 1, 0:-1] = linalg.spsolve(As, b)
+        self.sol[:, -1] = self.sol[:, 0]
+        return 0
+
+    def cn_liearized_2(self):
         self.sol[0, :] = self.f1(self.xi)
         A = self.tridiag(self.r / 2, 1.0j - self.r, + self.r / 2, self.M)
         B_const = self.tridiag(- self.r / 2, 1.0j + self.r, - self.r / 2, self.M)
         A[0, -1] = self.r / 2
         A[-1, 0] = self.r / 2
-        As = sparse.csr_matrix(A)
         B_const[0, -1] = - self.r / 2
         B_const[-1, 0] = - self.r / 2
+        As = sparse.csr_matrix(A)
 
         for i in range(0, self.N):
-            abs = np.absolute(self.sol[i, 0:-1])**2
-            B = (3 / 2) * np.diag(abs * 0.0j, 0) - np.diag(np.absolute(self.sol[i, 0:-1] * np.absolute(self.sol[i-1, 0:-1])), 0)
-            B *= self.lmbda * self.k
+            abs = np.absolute(self.sol[i, 0:-1])
+            B = np.diag(5 * abs**2 * 0.0j - 2 * abs * np.absolute(self.sol[i - 1, 0:-1]) * 1.0j, 0)
+            B *= 0.5 * self.lmbda * self.k
             B += B_const
             b = np.matmul(B, self.sol[i, 0:-1])
+            b -= 0.5 * self.lmbda * self.k * abs**2 * self.sol[i - 1, 0:-1]
             self.sol[i + 1, 0:-1] = linalg.spsolve(As, b)
         self.sol[:, -1] = self.sol[:, 0]
         return 0
@@ -123,17 +154,22 @@ class Nonlin_Scrodinger_Solver:
 
 
 if __name__ == '__main__':
-    cn_explicit_average = Nonlin_Scrodinger_Solver([-np.pi, np.pi], 2, 200, 200, 5, 'cn_explicit_average.pkl')
-    cn_explicit_average.cn_explicit_average()
-    cn_explicit_average.plot()
+    # cn_explicit_average = Nonlin_Scrodinger_Solver([-np.pi, np.pi], 2, 400, 400, 5, 'cn_explicit_average.pkl')
+    # cn_explicit_average.cn_explicit_average()
+    # cn_explicit_average.plot()
+    # cn_explicit_average.plot_analytic()
 
     # cn_implicit = Nonlin_Scrodinger_Solver([-np.pi, np.pi], 2, 200, 200, 5, 'cn_implicit.pkl')
     # cn_implicit.cn_implicit()
     # cn_implicit.plot()
 
-    cn_linearized = Nonlin_Scrodinger_Solver([-np.pi, np.pi], 2, 200, 200, 5, 'cn_linearized.pkl')
-    cn_linearized.cn_liearized()
-    cn_linearized.plot()
+    cn_linearized_1 = Nonlin_Scrodinger_Solver([-np.pi, np.pi], 2, 200, 200, 5, 'cn_linearized.pkl')
+    cn_linearized_1.cn_liearized_1()
+    cn_linearized_1.plot()
+
+    cn_linearized_2 = Nonlin_Scrodinger_Solver([-np.pi, np.pi], 2, 200, 200, 5, 'cn_linearized.pkl')
+    cn_linearized_2.cn_liearized_2()
+    cn_linearized_2.plot()
 
 
 
